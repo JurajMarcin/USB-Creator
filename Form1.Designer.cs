d12cb0ad46c55372aa2c965279203a1bc995e05b
Juraj Marcin <juraj@jurajmarcin.com>
2018-03-31 21:45:16 +0200

    added project

---
+++ Form1.Designer.cs
@@ -0,0 +1,272 @@
+﻿namespace USB_Creator {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.files = new System.Windows.Forms.ListView();
+            this.addBtn = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.remBtn = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.changeName = new System.Windows.Forms.CheckBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tries = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.eject = new System.Windows.Forms.CheckBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.tries)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // files
+            // 
+            this.files.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.files.Location = new System.Drawing.Point(6, 19);
+            this.files.Name = "files";
+            this.files.Size = new System.Drawing.Size(366, 350);
+            this.files.TabIndex = 0;
+            this.files.UseCompatibleStateImageBehavior = false;
+            this.files.View = System.Windows.Forms.View.List;
+            // 
+            // addBtn
+            // 
+            this.addBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.addBtn.Location = new System.Drawing.Point(378, 19);
+            this.addBtn.Name = "addBtn";
+            this.addBtn.Size = new System.Drawing.Size(75, 23);
+            this.addBtn.TabIndex = 1;
+            this.addBtn.Text = "Add file";
+            this.addBtn.UseVisualStyleBackColor = true;
+            this.addBtn.Click += new System.EventHandler(this.addBtn_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.remBtn);
+            this.groupBox1.Controls.Add(this.files);
+            this.groupBox1.Controls.Add(this.addBtn);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(459, 375);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Files to copy";
+            // 
+            // remBtn
+            // 
+            this.remBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.remBtn.Location = new System.Drawing.Point(378, 48);
+            this.remBtn.Name = "remBtn";
+            this.remBtn.Size = new System.Drawing.Size(75, 23);
+            this.remBtn.TabIndex = 2;
+            this.remBtn.Text = "Remove file";
+            this.remBtn.UseVisualStyleBackColor = true;
+            this.remBtn.Click += new System.EventHandler(this.remBtn_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Multiselect = true;
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // changeName
+            // 
+            this.changeName.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.changeName.AutoSize = true;
+            this.changeName.Location = new System.Drawing.Point(12, 398);
+            this.changeName.Name = "changeName";
+            this.changeName.Size = new System.Drawing.Size(129, 17);
+            this.changeName.TabIndex = 4;
+            this.changeName.Text = "Change volume name";
+            this.changeName.UseVisualStyleBackColor = true;
+            this.changeName.CheckedChanged += new System.EventHandler(this.changeName_CheckedChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.textBox1.Location = new System.Drawing.Point(147, 396);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(170, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(742, 393);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Copy files";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(554, 399);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Try to copy";
+            // 
+            // tries
+            // 
+            this.tries.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.tries.Location = new System.Drawing.Point(611, 397);
+            this.tries.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.tries.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.tries.Name = "tries";
+            this.tries.Size = new System.Drawing.Size(69, 20);
+            this.tries.TabIndex = 8;
+            this.tries.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(686, 399);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "times";
+            // 
+            // eject
+            // 
+            this.eject.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.eject.AutoSize = true;
+            this.eject.Checked = true;
+            this.eject.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.eject.Location = new System.Drawing.Point(323, 398);
+            this.eject.Name = "eject";
+            this.eject.Size = new System.Drawing.Size(165, 17);
+            this.eject.TabIndex = 10;
+            this.eject.Text = "Automatically eject USB drive";
+            this.eject.UseVisualStyleBackColor = true;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStatus});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 419);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(829, 22);
+            this.statusStrip1.TabIndex = 11;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStatus
+            // 
+            this.toolStatus.Name = "toolStatus";
+            this.toolStatus.Size = new System.Drawing.Size(60, 17);
+            this.toolStatus.Text = "toolStatus";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Controls.Add(this.richTextBox1);
+            this.groupBox2.Location = new System.Drawing.Point(477, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(340, 375);
+            this.groupBox2.TabIndex = 12;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Log";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(6, 19);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(328, 350);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(829, 441);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.eject);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.changeName);
+            this.Controls.Add(this.tries);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.MinimumSize = new System.Drawing.Size(666, 300);
+            this.Name = "Form1";
+            this.Text = "USB Creator";
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.tries)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView files;
+        private System.Windows.Forms.Button addBtn;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button remBtn;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.CheckBox changeName;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown tries;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox eject;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStatus;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,98 @@
+﻿using RemoveDriveByLetter;
+using System;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Windows.Forms;
+
+namespace USB_Creator {
+	public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+            textBox1.Enabled = changeName.Checked;
+            toolStatus.Text = "Ready";
+        }
+
+        private void addBtn_Click(object sender, EventArgs e) {
+            openFileDialog1.ShowDialog();
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e) {
+            foreach (string file in openFileDialog1.FileNames) {
+                files.BeginUpdate();
+                files.Items.Add(file);
+                files.EndUpdate();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            toolStatus.Text = "Working";
+            this.Cursor = Cursors.WaitCursor;
+            DriveInfo[] usbs = DriveInfo.GetDrives();
+            foreach (DriveInfo usb in usbs) {
+                if (usb.IsReady && usb.DriveType == DriveType.Removable) {
+                    if (changeName.Checked) {
+                        usb.VolumeLabel = textBox1.Text;
+                    }
+                    bool error = false;
+                    foreach (ListViewItem file in files.Items) {
+                        FileInfo fileInfo = new FileInfo(file.Text);
+                        if (fileInfo.Exists) {
+                            bool same = false;
+                            int i = 0;
+                            while (!same) {
+                                i++;
+                                if (i > tries.Value) {
+                                    error = true;
+                                    break;
+                                }
+                                File.Copy(fileInfo.FullName, usb.RootDirectory + fileInfo.Name, true);
+                                Console.WriteLine("COPIED");
+                                same = CheckFiles(fileInfo.FullName, usb.RootDirectory + fileInfo.Name);
+                            }
+                            if (error) {
+                                richTextBox1.AppendText("Failed to copy " + fileInfo.Name + " to  drive " + usb.Name + "\n");
+                            } else {
+                                richTextBox1.AppendText(fileInfo.Name + " has been copied to  drive " + usb.Name + "\n");
+                            }
+                        }
+                    }
+                    if (eject.Checked && !error) {
+                        RemoveDriveTools.RemoveDrive(usb.Name.Replace("\\", ""));
+                    }
+                }
+            }
+            toolStatus.Text = "Ready";
+            this.Cursor = Cursors.Arrow;
+        }
+
+        private void remBtn_Click(object sender, EventArgs e) {
+            foreach (ListViewItem selItem in files.SelectedItems) {
+                files.Items.Remove(selItem);
+            }
+        }
+
+        private void changeName_CheckedChanged(object sender, EventArgs e) {
+            textBox1.Enabled = changeName.Checked;
+        }
+
+        public bool CheckFiles(string fileName, string fileName2) {
+            using (var md5 = MD5.Create()) {
+                try {
+                    byte[] hash1;
+                    byte[] hash2;
+                    using (var stream = File.OpenRead(fileName)) {
+                        hash1 = md5.ComputeHash(stream);
+                    }
+                    using (var stream = File.OpenRead(fileName2)) {
+                        hash2 = md5.ComputeHash(stream);
+                    }
+                    return hash1.SequenceEqual(hash2);
+                } catch (IOException) {
+                    return false;
+                }
+            }
+        }
+    }
+}
